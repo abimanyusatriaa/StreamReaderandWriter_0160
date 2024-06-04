948c7dec0253abeea816e5248c236e24fc0ade46
abimanyusatriaa <adji3890@gmail.com>
2024-06-04 11:10:55 +0700

    int main

--- exeptionStandard/exeptionStandard.cpp
+++ exeptionStandard/exeptionStandard.cpp
@@ -7,6 +7,17 @@ using namespace std;
 
 int main()
 {
-    
+    cout << "Awal Program" << endl; //penanda 1
+    try {
+        array<int, 3> data = { 1,2,3 };
+    }
+    catch (exception& e) {
+        //penangkap menggunakan objek exeption
+        cout << e.what() << endl;
+        /*akan dieksekusi karena array data hanyaa memiliki 3 elemen*/
+    }
+    cout << "Baris Program yang Terakhir" << endl;
+    /*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+    return 0;
 }
 
