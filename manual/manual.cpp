a2953a89ef26ac7f3c382a82239e42f5ed743f99
abimanyusatriaa <adji3890@gmail.com>
2024-06-04 11:00:26 +0700

    try

--- manual/manual.cpp
+++ manual/manual.cpp
@@ -3,5 +3,12 @@ using namespace std;
 
 int main()
 {
-    
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        //throw 0.5; //melemparkan sebuat integer maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a) {
+
+    }
 }
