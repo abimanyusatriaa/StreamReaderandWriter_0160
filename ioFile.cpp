8dedd62ba6c0bf0130fb3d81ab59120b22161cd4
abimanyusatriaa <adji3890@gmail.com>
2024-06-04 10:27:45 +0700

    membuka dan membaca file hika ada dan tidak ditemukan file

--- ioFile.cpp
+++ ioFile.cpp
@@ -27,4 +27,27 @@ int main() {
 	}
 	//selesai dalam menulis sekarang tutup file nya
 	outfile.close();
+
+	//membuka file dalam mode membaca
+	ifstream infile;
+	//menunjuk ke dalam satu file
+	infile.open("contohfile.txt");
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	//jika ada file maka
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			//dan tampilkan disini
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
+	//jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
 }
+
